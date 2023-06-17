af13b8e2667a34deba5e888a5adda8d730140081
is-this-echo <djpool007@gmail.com>
2023-06-18 01:59:57 +0530

    connect auth service to mongodb instance for the signup process
    Implemented password hashing in auth service using mongodb pre-save
    hooks for the signup process, created a new error to handle bad
    requests. Also, the cluster deployment and service yaml config for
    auth-mongodb instance is also :setup.

---
+++ auth/src/models/user.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+/*
+This interface describes the properties that are
+required to create a new User in the collection
+*/
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+/*
+An interface that describes the properties
+a User model should have
+*/
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+/*
+An interface that describes the properties
+that a User document has
+*/
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -22,11 +23,17 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    // const { email, password } = req.body;
-    console.log("Creating an user..");
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("Email already in use...");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 

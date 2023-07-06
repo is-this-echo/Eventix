0ca8a255317b0941b07ea7471a13ff7a0fe4ee41
is-this-echo <djpool007@gmail.com>
2023-07-07 02:19:22 +0530

    Setup testing env & write test-cases for signup route
    The testing env is setup using jest & supertest. Modified the
    dockerfile to not install dev dependencies in pkg.json. For ease of
    testing app.ts file is created to separate port and express app logic.
    Some basic unit tests are added for auth-service.

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Returns a 201 status-code on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("Returns a 400 status-code with an invalid email signup ", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@tecom",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("Returns a 400 status-code with an invalid password signup ", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@te.com",
+      password: "0idp",
+    })
+    .expect(400);
+});
+
+it("Returns a 400 status-code with missing email & password signup ", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "opop@anime.com",
+      password: "",
+    })
+    .expect(400);
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "",
+      password: "9d0asfwsfj23--1",
+    })
+    .expect(400);
+});

9cf027065f77a0195c9194687054273fb66206a8
is-this-echo <djpool007@gmail.com>
2023-07-30 22:31:46 +0530

    create new tickets-service : configure cluster settings, dockerfile etc

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@echoeventix/common";
+
+const app = express();
+
+// allows express to trust the proxy connection created by ingress-nginx controller
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Has a route handler listening to /api/tickets for post requests", async () => {});
+it("Can only be accessed if user is signed in", async () => {});
+
+it("Returns an error if an invalid title is provided", async () => {});
+
+it("Returns an error if an invalid price is provided", async () => {});
+
+it("Returns an error if an invalid title is provided", async () => {});
